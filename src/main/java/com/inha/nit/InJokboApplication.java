874dc1e6968485b12cd4c2af7fff59115bc3ad3d
Bin <baebine@naver.com>
2023-03-24 22:26:11 +0900

    .NTP - Team D

---
+++ src/main/java/com/inha/nit/InJokboApplication.java
@@ -0,0 +1,13 @@
+package com.inha.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InJokboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InJokboApplication.class, args);
+    }
+
+}
